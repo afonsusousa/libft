f983864657a5777467a7bcba3d7f76392ba9d060
afonsusousa <afonso.rocha@outlook.com>
2025-04-11 20:12:19 +0100

    fixed split and protected it, whatever that emans

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,20 +6,20 @@
 /*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:44:36 by amagno-r          #+#    #+#             */
-/*   Updated: 2025/04/11 17:34:59 by amagno-r         ###   ########.fr       */
+/*   Updated: 2025/04/11 19:19:00 by amagno-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	ft_count_words(char *str, const char sep)
+static int	ft_count_words(const char *str, const char sep)
 {
 	size_t i;
 	int	words;
 
 	i = 0;
-	words = (str[0] != sep) || !str[0];
+	words = (str[0] != sep) && (str[0] != 0);
 	while(str[i])
 	{
 		if(str[i] == sep && str[i + 1] != sep && str[i + 1])
@@ -29,46 +29,54 @@ static int	ft_count_words(char *str, const char sep)
 	return (words);
 }
 
-static	char *ft_get_word(char *start, const char sep, size_t *index)
+static	int ft_strlen_sep(const char *start, const char sep)
 {
-	size_t	i;
-	char	*ret;
-
+	int	i;
+	
 	i = 0;
 	while(start[i] != sep && start[i])
 		i++;
-	*index += i;
-	return (ft_substr(start, 0, i));
+	return (i);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	word;
-	char	*str;
+	size_t	word_len;
 	char	**ret;
 
 	i = 0;
 	word = 0;
-	str = (char *) s;
-	ret = (char **)malloc(ft_count_words(str, c) * sizeof(char *));
+	if(!s)
+		return (0);
+	ret = (char **)ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
 	if(!ret)
 		return (NULL);
-	while(str[i])
+	while(s[i])
 	{
-		if(str[i] != c && str[i])
-			ret[word] = ft_get_word(&str[i], c, &i);
-		if(!str[i])
-			break;
-		i++;
+		word_len = 0;
+		if(s[i] != c && s[i])
+		{
+			word_len = ft_strlen_sep(&s[i], c);
+			ret[word++] = ft_substr(s, i, word_len);
+			i += word_len;
+		}
+		else
+			i++;
 	}
 	return (ret);
 }
 
+/*
 #include <stdio.h>
 int main(int argc, char **argv)
 {
-	char **split = ft_split(argv[1], ' ');
-	printf("%s\n", split[1]);
+	char **split = ft_split("\0aa\0bbb", '\0');
+	for(int i = 0; i < ft_count_words("\0aa\0bbb", '\0'); i++)
+		printf("letra qualquer %s\n", split[i]);
+//	char **split = ft_split(argv[1], argv[2][0]);
+//	for(int i = 0; i < ft_count_words(argv[1], argv[2][0]); i++)
+//		printf("%s\n", split[i]);
 }
-
+*/
--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:01:24 by amagno-r          #+#    #+#             */
-/*   Updated: 2025/04/10 22:31:20 by amagno-r         ###   ########.fr       */
+/*   Updated: 2025/04/11 19:20:35 by amagno-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	src_len = (unsigned int )ft_strlen((char *)s);
 	if(!s)
-		return (NULL);
+		return (0);
 	if(start >= src_len)
 		return ((char *)ft_calloc(1, 1));
 	if(start + len > src_len)
