0244cdfb05f2bf398e7b8784bd91b6962fed6145
afonsusousa <afonso.rocha@outlook.com>
2025-04-12 22:48:20 +0100

    lstmap working now

--- bonus/ft_lstmap.c
+++ bonus/ft_lstmap.c
@@ -6,25 +6,26 @@
 /*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 21:18:57 by amagno-r          #+#    #+#             */
-/*   Updated: 2025/04/12 21:42:09 by amagno-r         ###   ########.fr       */
+/*   Updated: 2025/04/12 22:27:54 by amagno-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*temp;
 	t_list	*ret;
 
+	ret = NULL;
 	while(lst)
 	{
-		temp = ft_lstnew(lst->content);
-		if (!(*f)(temp))
-			(*del)(temp);
+		temp = ft_lstnew((*f)(lst->content));
+		if (!temp)
+			ft_lstdelone(temp, del);
 		else
-			ft_lstadd_back(&ret, (*f)(temp));
+			ft_lstadd_back(&ret, temp);
 		lst = lst->next;
 	}
 	return (ret);
